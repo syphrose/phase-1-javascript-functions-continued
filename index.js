3fe647d1203fed59b1719c2e05210c664e533c75
syphrose <syphrose.wabulwenyi@student.moringaschool.com>
2022-06-10 23:24:09 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(message='roller-skate'){
+    return `This Saturday, I want to ${message}!`
+};
+// saturdayFun('bathe my dog');
+
+function mondayWork(message='go to the office'){
+    return `This Monday, I will ${message}.`
+};
+// mondayWork('work from home');
+
+let wrapAdjective = function(message='*'){
+    return function (text='special'){
+        return `You are ${message}${text}${message}!`
+    }
+}
+
